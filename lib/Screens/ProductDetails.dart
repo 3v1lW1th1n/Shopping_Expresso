e4fac823584c9414759c8c8a271d6da01d56bd91
Abhishek Kumar <iamabhishek229313@gmail.com>
2020-03-07 15:29:50 +0530

    Optimized ProductDetails

--- lib/Screens/ProductDetails.dart
+++ lib/Screens/ProductDetails.dart
@@ -366,7 +366,7 @@ class _ProductDetailsState extends State<ProductDetails> {
                             ),
                             new SizedBox(height: ScreenUtil().setHeight(20.0)),
                             new Text(
-                              "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.",
+                              "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.\n Eget mi proin sed libero enim sed faucibus. Morbi tristique senectus et netus. Dolor sit amet consectetur adipiscing elit ut aliquam purus sit.Id velit ut tortorpretium viverra.",
                             )
                           ],
                         ),
